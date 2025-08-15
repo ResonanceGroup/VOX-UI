97ce101f54a3d31ac6d26d1141bb1cf10217bd5f
Jason Owens <67338327+Jdo300@users.noreply.github.com>
2025-08-15 12:02:17 -0400

    Added function node to control display of orb overyal in Dashboard

--- node-red-orb-notification.js
+++ node-red-orb-notification.js
@@ -783,7 +783,7 @@
             }
             
             .overlay-dark .status-icon {
-                color: #81C784 !important; /* Match original VOX-UI dark mode idle color */
+                color: #4CAF50 !important; /* Use same green as light mode for recording dot */
                 display: inline-block !important;
             }
             
